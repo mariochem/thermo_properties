442bd7761fc3af61dd3e38075e821b1407d9b016
Mario Edson de Almeida <MARIO.E.ALMEIDA@GMAIL.COM>
2021-03-02 14:08:34 -0300

    Add files via upload

--- vls_t.cmd
+++ vls_t.cmd
@@ -2,4 +2,7 @@ set title "Specific Volume of Saturated Liquid"
 set xlabel "Specific Volume(m3/kg)"
 set ylabel "Temp(C)"
 set grid
-plot "pvap.txt" using 3:1 title "SVL" with lines, "pvap.txt" using 3:1 title "Exp." with points
+set xrange [0.00095:0.0032]
+set yrange [0.:380.]
+plot "pvap.txt" using 3:1 title "Exp. SVL" with linespoints
+
